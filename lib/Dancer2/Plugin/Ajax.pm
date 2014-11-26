d2d92d9a67d2d2f4307f6500d986d85ef5965046
Sawyer X <xsawyerx@cpan.org>
2014-11-26 23:13:01 +0000

    Import of XSAWYERX/Dancer2-Plugin-Ajax-0.200000.tar.gz from CPAN.
    gitpan-cpan-distribution: Dancer2-Plugin-Ajax
    gitpan-cpan-version:      0.200000
    gitpan-cpan-path:         XSAWYERX/Dancer2-Plugin-Ajax-0.200000.tar.gz
    gitpan-cpan-author:       XSAWYERX
    gitpan-cpan-maturity:     released

---
+++ lib/Dancer2/Plugin/Ajax.pm
@@ -0,0 +1,130 @@
+package Dancer2::Plugin::Ajax;
+# ABSTRACT: a plugin for adding Ajax route handlers
+$Dancer2::Plugin::Ajax::VERSION = '0.200000';
+use strict;
+use warnings;
+use Dancer2::Plugin;
+
+register 'ajax' => sub {
+    my ( $dsl, $pattern, @rest ) = @_;
+
+    my $default_methods = [ 'get', 'post' ];
+
+    # If the given pattern is an ArrayRef, we override the defaults
+    # and pass these onto to DSL->any()
+    if( ref($pattern) eq "ARRAY" ) {
+        $default_methods = $pattern;
+        $pattern = shift(@rest);
+    }
+
+    my $code;
+    for my $e (@rest) { $code = $e if ( ref($e) eq 'CODE' ) }
+
+    my $content_type = plugin_setting->{content_type} || 'text/xml';
+
+    my $ajax_route = sub {
+
+        # # must be an XMLHttpRequest
+        if ( not $dsl->request->is_ajax ) {
+            $dsl->pass and return 0;
+        }
+
+        # Default response content type is either what's defined in the
+        # plugin setting or text/xml
+        $dsl->response->header('Content-Type')
+          or $dsl->response->content_type( $content_type );
+
+        # disable layout
+        my $layout = $dsl->setting('layout');
+        $dsl->setting( 'layout' => undef );
+        my $response = $code->();
+        $dsl->setting( 'layout' => $layout );
+        return $response;
+    };
+
+    $dsl->any( $default_methods => $pattern, $ajax_route );
+};
+
+register_plugin;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dancer2::Plugin::Ajax - a plugin for adding Ajax route handlers
+
+=head1 VERSION
+
+version 0.200000
+
+=head1 SYNOPSIS
+
+    package MyWebApp;
+
+    use Dancer2;
+    use Dancer2::Plugin::Ajax;
+
+    # For GET / POST
+    ajax '/check_for_update' => sub {
+        # ... some Ajax code
+    };
+
+    # For all valid HTTP methods
+    ajax ['get', 'post', ... ] => '/check_for_more' => sub {
+        # ... some Ajax code
+    };
+
+    dance;
+
+=head1 DESCRIPTION
+
+The C<ajax> keyword which is exported by this plugin allow you to define a route
+handler optimized for Ajax queries.
+
+The route handler code will be compiled to behave like the following:
+
+=over 4
+
+=item *
+
+Pass if the request header X-Requested-With doesn't equal XMLHttpRequest
+
+=item *
+
+Disable the layout
+
+=item *
+
+The action built matches POST / GET requests by default. This can be extended by passing it an ArrayRef of allowed HTTP methods.
+
+=back
+
+=head1 CONFIGURATION
+
+By default the plugin will use a content-type of 'text/xml' but this can be overridden
+with plugin setting 'content_type'.
+
+Here is example to use JSON:
+
+  plugins:
+    Ajax:
+      content_type: 'application/json'
+
+=head1 AUTHOR
+
+Dancer Core Developers
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Alexis Sukrieh.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ t/author-no-tabs.t
@@ -0,0 +1,26 @@
+
+BEGIN {
+  unless ($ENV{AUTHOR_TESTING}) {
+    require Test::More;
+    Test::More::plan(skip_all => 'these tests are for testing by the author');
+  }
+}
+
+use strict;
+use warnings;
+
+# this test was generated with Dist::Zilla::Plugin::Test::NoTabs 0.09
+
+use Test::More 0.88;
+use Test::NoTabs;
+
+my @files = (
+    'lib/Dancer2/Plugin/Ajax.pm',
+    't/00-compile.t',
+    't/00-report-prereqs.dd',
+    't/00-report-prereqs.t',
+    't/ajax.t'
+);
+
+notabs_ok($_) foreach @files;
+done_testing;
